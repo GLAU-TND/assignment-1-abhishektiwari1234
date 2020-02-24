a98ab72a7991f2d4819e5791455f5825f877f731
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-02-24 22:04:37 +0530

    For Searching Contact

--- src/LinkedList/Action.java
+++ src/LinkedList/Action.java
@@ -92,4 +92,15 @@ public class Action {
         }
     }
 
+    public void searchContact(newLinkedList linkedList1) {
+        System.out.println("You could search for a contact from their first names: ");
+        String firstName = scanner.nextLine().trim();
+        if (linkedList1.indexOf(firstName) >= 0) {
+            System.out.println("Match found");
+            linkedList1.getNode(linkedList1.indexOf(firstName));
+        } else {
+            System.out.println("No match found");
+        }
+    }
+
 }
